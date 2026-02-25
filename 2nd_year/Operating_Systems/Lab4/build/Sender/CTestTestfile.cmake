085a4881caf5c6524d3d72ebda04b48ef3b5f790
Supreme Intelligence MacOS <supremeintelligence1602@mail.ru>
2026-02-25 16:38:24 +0300

    Removed CMake build artifacts from 2nd year Operating Systems Lab4 repo. Updated .gitignore.

--- 2nd_year/Operating_Systems/Lab4/build/Sender/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/IT/WIN32API/OS_Lab4/Sender
-# Build directory: D:/IT/WIN32API/OS_Lab4/build/Sender
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 2nd_year/Operating_Systems/Lab4/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/IT/WIN32API/OS_Lab4/tests
-# Build directory: D:/IT/WIN32API/OS_Lab4/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
